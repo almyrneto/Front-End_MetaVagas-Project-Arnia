c266ef8f3ea80d9701e398a0b3ee3d54933f87ee
Jessie <jessie.moura19@gmail.com>
2023-10-25 19:20:11 -0300

    fix: added content-type to axios config

--- MetaVagas/src/services/config.ts
+++ MetaVagas/src/services/config.ts
@@ -2,4 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: "https://metavagasbackend-production.up.railway.app/v1",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
