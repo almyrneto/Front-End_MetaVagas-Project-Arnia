810a7226441b2095165b76b5eedeb9f1ea1ed69b
almyrneto <almyr_neto@hotmail.com>
2023-10-26 00:20:58 -0300

    bug fix, finished ContainerSearch of the logged in page, added footer to all pages

--- MetaVagas/src/pages/login.tsx
+++ MetaVagas/src/pages/login.tsx
@@ -1,4 +1,5 @@
 import { BannerLogin } from "../components/bannerLogin";
+import { Footer } from "../components/footer";
 import { Header } from "../components/header";
 import { TutorialContent } from "../components/tutorialContent";
 
@@ -7,9 +8,10 @@ import { TutorialContent } from "../components/tutorialContent";
 export const Login = () => {
     return (
         <>
-           <Header />
-           <BannerLogin />
-           <TutorialContent />
+            <Header />
+            <BannerLogin />
+            <TutorialContent />
+            <Footer className={""} />
         </>
     )
 }
